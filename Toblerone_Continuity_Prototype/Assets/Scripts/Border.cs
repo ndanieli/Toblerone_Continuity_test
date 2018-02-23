463c91363ac1a658d601b87e41756a83b1df7b2f
ndanieli <ndanieli@gmail.com>
2018-02-23 12:43:12 +0200

    Fix IsTrigger Bug
    Bug -
    If the hero was unable to move between borders at a specific time, it would disable the border completely (preventing future use of the border, regardless of the adjacent frame).
    
    Background for the bug -
    In order to prevent the hero from going through the borders (instead of moving between frames), the isTrigger property had to be set as "false" once the trigger was activated.
    the isTrigger property was set to be enabled again once the OnExit trigger occured, however, in some cases it didn't work, which lef the property as "false".
    
    Fix -
    Duplicated the borders:
    From the original one, removed the script and triggers.
    From the new one, removed the mesh (so it's invisible).
    
    Removed the isTrigger changes in the script.

--- Toblerone_Continuity_Prototype/Assets/Scripts/Border.cs
+++ Toblerone_Continuity_Prototype/Assets/Scripts/Border.cs
@@ -2,24 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Border : MonoBehaviour {
+public class Border : MonoBehaviour
+{
 
     public int borderSide;
     public Frame frame;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-      //  Debug.Log("Enter");
-        gameObject.GetComponent<BoxCollider2D>().isTrigger = false;
         if (other.tag.Equals("Player"))
         {
             frame.OnBorderTrigger(borderSide);
         }
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-//        Debug.Log("Exit");
-        gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
-    }
 }
