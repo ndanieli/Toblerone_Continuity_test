06ed135d6d516a1e2caf6851462f015dc0849ec8
Tom Peri <tomp228@gmail.com>
2018-03-11 12:41:33 +0200

    Added border labels to scene view
    - Added a text mesh which behind the camera view (therfore not rendered in game view).
    - Edited Border script to change the  text upon starting the game

--- Toblerone_Continuity_Prototype/Assets/Scripts/Frame Scripts/Border.cs
+++ Toblerone_Continuity_Prototype/Assets/Scripts/Frame Scripts/Border.cs
@@ -8,6 +8,15 @@ public class Border : MonoBehaviour
     public int borderSide;
     public Frame frame;
 
+    void Start()
+    {
+        TextMesh label = GetComponentInChildren<TextMesh>();
+        if (label != null)
+        {
+            label.text = "" + frame.borderLabels[borderSide];
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag.Equals("Player"))
